3113553a20f460751ce88ed620f1cc3ed9955a42
oyewoledavid <64299405+oyewoledavid@users.noreply.github.com>
2022-09-02 09:26:38 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,98 @@
+document.getElementById('one').onclick = one
+document.getElementById('two').onclick = two
+document.getElementById('three').onclick = three
+document.getElementById('four').onclick = four
+document.getElementById('five').onclick = five 
+document.getElementById('six').onclick = six
+document.getElementById('seven').onclick = seven
+document.getElementById('eight').onclick = eight
+document.getElementById('nine').onclick = nine
+document.getElementById('ten').onclick = ten
+document.getElementById('eleven').onclick = eleven
+document.getElementById('twelve').onclick = twelve
+document.getElementById('thirteen').onclick = thirteen
+document.getElementById('fourteen').onclick = fourteen 
+document.getElementById('fifteen').onclick = fifteen
+document.getElementById('sixteen').onclick = sixteen
+document.getElementById('seventeen').onclick = seventeen
+document.getElementById('eighteen').onclick = eighteen
+document.getElementById('nineteen').onclick = nineteen
+document.getElementById('twenty').onclick = twenty
+
+
+function one() {
+document.getElementById("demo").style.backgroundImage = "url('nature gif/One.jpg')";
+}
+function two() {
+  document.getElementById("demo").style.backgroundImage = "url('nature gif/Two.jpeg')";
+}
+function three() {
+  document.getElementById("demo").style.backgroundImage =
+    "url('nature gif/Three.jpeg')";
+}
+function four() {
+  document.getElementById('demo').style.backgroundImage = "url('nature gif/Four.jpeg')";
+}
+function five() {
+  document.getElementById('demo').style.backgroundImage = "url('nature gif/Five.gif')";
+}
+function six() {
+  document.getElementById('demo').style.backgroundImage =
+    "url('nature gif/Six.jpeg')";
+}
+function seven() {
+  document.getElementById("demo").style.backgroundImage =
+    "url('nature gif/Seven.jpeg')";
+}
+function eight() {
+  document.getElementById("demo").style.backgroundImage =
+    "url('nature gif/Eight.jpeg')";
+}
+function nine() {
+  document.getElementById("demo").style.backgroundImage =
+    "url('nature gif/Nine.jpeg')";
+}
+function ten() {
+  document.getElementById("demo").style.backgroundImage =
+    "url('nature gif/Ten.jpeg')";
+}
+function eleven() {
+  document.getElementById("demo").style.backgroundImage =
+    "url('nature gif/Eleven.jpeg')";
+}
+function twelve() {
+  document.getElementById("demo").style.backgroundImage =
+    "url('nature gif/Twelve.jpg')";
+}
+function thirteen() {
+  document.getElementById("demo").style.backgroundImage =
+    "url('nature gif/Thirteen.gif')";
+}
+function fourteen() {
+  document.getElementById("demo").style.backgroundImage =
+    "url('nature gif/Fourteen.gif')";
+}
+function fifteen() {
+  document.getElementById("demo").style.backgroundImage =
+    "url('nature gif/Fifteen.jpg')";
+}
+function sixteen() {
+  document.getElementById("demo").style.backgroundImage =
+    "url('nature gif/Sisteen.jpeg')";
+}
+function seventeen() {
+  document.getElementById("demo").style.backgroundImage =
+    "url('nature gif/Seventeen.jpeg')";
+}
+function eighteen() {
+  document.getElementById("demo").style.backgroundImage =
+    "url('nature gif/Eighteen.jpeg')";
+}
+function nineteen() {
+  document.getElementById("demo").style.backgroundImage =
+    "url('nature gif/Nineteen.gif')";
+}
+function twenty() {
+  document.getElementById("demo").style.backgroundImage =
+    "url('nature gif/Twenty.gif')";
+}
